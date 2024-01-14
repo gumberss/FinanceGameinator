41ce24d6dcc8877f3262a9b79f6e4c193ff5b38c
gumberss <gustavomarinsuppi@gmail.com>
2024-01-14 20:03:26 -0300

    chore: changing and adding some const strings

--- finance_gameinator/lib/components/constants/AppStrings.dart
+++ finance_gameinator/lib/components/constants/AppStrings.dart
@@ -4,19 +4,26 @@ class AppStrings {
   static const String loginAndRegister = 'Login and Register UI';
   static const String uhOhPageNotFound = 'uh-oh!\nPage not found';
   static const String register = 'Register';
+  static const String anErrorHappened = 'An Error happened';
   static const String login = 'Login';
   static const String createYourAccount = 'Create your account';
   static const String doNotHaveAnAccount = "Don't have an account?";
   static const String facebook = 'Facebook';
   static const String google = 'Google';
   static const String signInToYourNAccount = 'Sign in to your\nAccount';
+  static const String confirmYourAccount = 'Confirm your account';
+  static const String confirm = 'Confirm';
   static const String signInToYourAccount = 'Sign in to your Account';
   static const String iHaveAnAccount = 'I have an account?';
   static const String forgotPassword = 'Forgot Password?';
   static const String orLoginWith = 'or Login with';
 
+  static const String confirmationCode = 'Confirmation Code';
+
   static const String loggedIn = 'Logged In!';
   static const String registrationComplete = 'Registration Complete!';
+  static const String userRegistered = 'User Registered!';
+  static const String invalidRegistrationCode = 'Invalid Registration Code';
 
   static const String name = 'Name';
   static const String pleaseEnterName = 'Please, Enter Name';
