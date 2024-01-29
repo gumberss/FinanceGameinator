8659b14dce669e4dcfb1b240f167f7a6d4a3b027
gumberss <gustavomarinsuppi@gmail.com>
2024-01-29 20:23:22 -0300

    chore: removing logs

--- ServerSide/Modules/Players/FinanceGameinator.Players.UseCases/UseCases/PlayerUseCase.cs
+++ ServerSide/Modules/Players/FinanceGameinator.Players.UseCases/UseCases/PlayerUseCase.cs
@@ -2,26 +2,19 @@
 using FinanceGameinator.Players.Db.Interfaces.Repositories;
 using FinanceGameinator.Players.Domain.Models;
 using FinanceGameinator.Players.UseCases.Interfaces.UseCases;
-using Microsoft.Extensions.Logging;
 
 namespace FinanceGameinator.Players.UseCases.UseCases
 {
     public class PlayerUseCase : IPlayerUseCase
     {
-        private readonly ILogger<PlayerUseCase> _logger;
         readonly IPlayerRepository _playerRepository;
 
-        public PlayerUseCase(ILogger<PlayerUseCase> logger,
-            IPlayerRepository playerRepository)
+        public PlayerUseCase(IPlayerRepository playerRepository)
         {
-            _logger = logger;
             _playerRepository = playerRepository;
         }
 
         public Task<Result<Player, BusinessException>> FindPlayer(Guid playerId)
-        {
-            _logger.LogInformation("GANHAMO");
-            return _playerRepository.QueryPlayer(playerId);
-        }
+            => _playerRepository.QueryPlayer(playerId);
     }
 }
